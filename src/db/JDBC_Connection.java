b703ae3ffd1afd774b3eda09af9522923ee2b5a9
Sushmita-Payal <79054481+Sushmita-Payal@users.noreply.github.com>
2024-07-11 13:12:14 +0530

    Add files via upload

---
+++ src/db/JDBC_Connection.java
@@ -0,0 +1,19 @@
+package db;
+
+import services.Interface_JDBC_Connection;
+
+import java.sql.*;
+
+
+public class JDBC_Connection implements Interface_JDBC_Connection {
+
+    public Connection Connection2() throws Exception {
+        String dburl = "jdbc:mysql://localhost:3306/rto_office";
+        String dbuser = "root";
+        String dbpass = "Payal@123";
+        String driverName = "com.mysql.cj.jdbc.Driver";
+        Class.forName(driverName);
+        Connection con = DriverManager.getConnection(dburl, dbuser, dbpass);
+        return con;
+    }
+}
